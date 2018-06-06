6a67905718eafa344504f26a5f58c49993af6aca
akash <akashagarwal0403@gmail.com>
2018-06-07 02:27:20 +0530

    adding the static part

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import VideoSection from './VideoSection.js'
+import Services from "./Services.js";
+import Blog from "./Blogs.js";
+import UserReview from './UserReview.js';
 
 const HomePage = () => (
-	<div>
-	</div>
+	<Fragment>
+		<VideoSection />
+		<Services />
+		<Blog />
+		<UserReview />
+	</Fragment>
 );
 
 export default HomePage;
---
+++ src/components/UserReview.js
@@ -0,0 +1,110 @@
+import React, { Fragment } from 'react';
+
+const Review = ()=> (
+ <Fragment>
+   <section className="reviews-area section-gap">
+		<div className="container">
+			<div className="row section-title">
+				<h1>User Reviews</h1>
+				<p>Who are extremely in love with our platform.</p>
+			</div>
+			<div className="row">
+				<div className="col-lg-4 col-md-6">
+					<div className="single-review">
+						<h4>Shubham</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+
+						</p>
+						<div className="star">
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star"></span>
+							<span className="fa fa-star"></span>
+						</div>
+					</div>
+				</div>
+				<div className="col-lg-4 col-md-6">
+					<div className="single-review">
+						<h4>Saurabh Kabra</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+
+						</p>
+						<div className="star">
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star"></span>
+							<span className="fa fa-star"></span>
+						</div>
+					</div>
+				</div>
+				<div className="col-lg-4 col-md-6">
+					<div className="single-review">
+						<h4>Sparsh Choudhary</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+						</p>
+						<div className="star">
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star"></span>
+							<span className="fa fa-star"></span>
+						</div>
+					</div>
+				</div>
+				<div className="col-lg-4 col-md-6">
+					<div className="single-review">
+						<h4>Jon Banks</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+						</p>
+						<div className="star">
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star"></span>
+							<span className="fa fa-star"></span>
+						</div>
+					</div>
+				</div>
+				<div className="col-lg-4 col-md-6">
+					<div className="single-review">
+						<h4>Landon Houston</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+						</p>
+						<div className="star">
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star"></span>
+							<span className="fa fa-star"></span>
+						</div>
+					</div>
+				</div>
+				<div className="col-lg-4 col-md-6">
+					<div className="single-review">
+						<h4>Nelle Wade</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+						</p>
+						<div className="star">
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star checked"></span>
+							<span className="fa fa-star"></span>
+							<span className="fa fa-star"></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+ </Fragment>   
+)
+
+export default Review;
