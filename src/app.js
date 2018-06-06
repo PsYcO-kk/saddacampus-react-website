ea403c75f826c28450f48a51ddeb8fdfff1495d2
PsYcO-kk <kkshtj40@gmail.com>
2018-06-07 00:27:09 +0530

    Routes and Header added.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const jsx = (<p>Hello World</p>);
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import AboutUsPage from './components/AboutUsPage';
+import PrivacyPolicyPage from './components/PrivacyPolicyPage';
+import NotFoundPage from './components/NotFoundPage';
+
+import './styles/bootstrap.min.css';
+import './styles/poppins.css';
+import './styles/main.css';
+import './styles/fontawesome.min.css';
+import './styles/index.css';
+
+const jsx = (
+	<BrowserRouter>
+		<div>
+			<Header />
+            <Switch>
+                <Route path="/" component={HomePage} exact={true} />
+                <Route path="/about-us" component={AboutUsPage} />
+                <Route path="/privacy-policy" component={PrivacyPolicyPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+		</div>
+	</BrowserRouter>
+);
 
 ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+export default class Header extends React.Component {
+	state = {
+		isOpen: false
+	};
+	toggle = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	};
+	render = () => {
+		return (
+			<div>
+				<Navbar color="light" light expand="lg" className="bg-white border-bottom">
+					<div className="container">
+						<NavbarBrand href="/">
+							<img src="/assets/images/brand/16-9.png" width="50" className="d-inline-block align-top" alt="" />
+							&nbsp; Saddacampus
+						</NavbarBrand>
+						<NavbarToggler onClick={this.toggle} />
+						<Collapse isOpen={this.state.isOpen} navbar>
+							<Nav className="ml-auto" navbar>
+								<NavItem>
+									<NavLink to="/about-us" activeClassName="active" className="nav-link">About Us</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink to="/privacy-policy" activeClassName="active" className="nav-link">Privacy Policy</NavLink>
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</div>
+				</Navbar>
+			</div>
+		);
+	};
+}
