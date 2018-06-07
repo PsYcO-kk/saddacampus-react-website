87174f860f31d59483c0e82975454a9cec19d2a9
PsYcO-kk <kkshtj40@gmail.com>
2018-06-08 00:05:34 +0530

    NumberForm - fetch data from API added

--- src/components/homepage/forms/NumberForm.js
+++ src/components/homepage/forms/NumberForm.js
@@ -1,33 +1,75 @@
 import React from "react";
 
 export default class NumberForm extends React.Component {
-    DisplayReturn = () => {
-        if(this.props.display)
-            return "block";
-        else
-            return "none";
-    }
-
-    checkDisplay = {
-        "display" : this.DisplayReturn()
-    }
-
+	constructor(props){
+		super(props);
+		this.state = {
+			number: '',
+			error: ''
+		};
+	}
+	validateInput = () => {
+		return this.state.number.match(/^\d{10}$/);
+	}
+	handleNumberInput = (e) => {
+		const number = e.target.value;
+		if(!number || number.match(/^\d{1,10}$/)){
+			this.setState(() => ({ error: '', number: number }));
+		}
+	}
+	changeView = (message) => {
+		const newState = {
+			NumberForm: false,
+			OtpForm: true,
+			ProfileSettings: false,
+			number: this.state.number,
+			otp: '',
+			message: message
+		}
+		this.props.changeView(newState);
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+		const country_code = e.target.country_code.value;
+		const number = this.state.number;
+		if(this.validateInput()){
+			fetch('http://127.0.0.1:3000/v1/membership/otp/'+country_code+'/'+number)
+			.then((response) => response.json())
+			.then((data) => {
+				if(data.success){
+					this.setState(() => ({ error: '' }));
+					this.changeView(data.message);
+				}
+				else{
+					this.setState(() => ({ error: data.message }));
+				}
+			})
+			.catch((error) => {
+				this.setState(() => ({ error }));
+			});
+		}
+		else{
+			document.getElementsByClassName('alert').className += ' alert-danger';
+			this.setState(() => ({
+				error: 'Please enter a valid mobile number.'
+			}));
+		}
+	}
     render(){
         return(
-            <form style={this.checkDisplay}>
-				<label id="inputLabel" htmlFor="numberInput">Enter your mobile number to continue:</label>
-				<div id="telephoneDiv" className="input-group mb-3">
-					<select className="custom-select form-control" defaultValue="+91" id="country_code">
+            <form onSubmit={this.handleSubmit}>
+				{this.state.error && <div className="alert alert-danger">{this.state.error}</div>}
+				<label htmlFor="numberInput">Enter your mobile number to continue:</label>
+				<div className="input-group mb-3">
+					<select className="custom-select form-control" defaultValue="+91" name="country_code">
 						<option value="+91">+91</option>
 					</select>
-					<input type="tel" className="form-control" id="numberInput" aria-describedby="numberHelp" minLength="10" maxLength="10" placeholder="# 7777 777777" />
+					<input type="tel" className="form-control" name="numberInput" value={this.state.number} onChange={this.handleNumberInput} placeholder="# 7777-777777" />
 					<div className="input-group-append">
-						<button id="go" className="btn btn-outline-secondary" type="button">Go</button>
+						<button className="btn btn-outline-secondary" type="submit">Go</button>
 					</div>
 				</div>
-				<br />
-				<span id="numberAlert" className="alert alert-danger"></span>
-				<small id="numberHelp" className="form-text text-muted">Don't worry. We won't stalk you.</small>
+				<small className="form-text text-muted">Don't worry. We won't stalk you.</small>
 			</form>
         );
     }
